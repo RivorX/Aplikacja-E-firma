98967bd66f4b95b0f59674e50096fab535c1b2cd
RivorX <rafilix11@gmail.com>
2024-04-14 17:00:16 +0200

    Dodanie podstron

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,23 +1,55 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from './views/Dashboard.jsx';
+
 import Login from './views/Login.jsx';
 import GuestLayout from './views/GuestLayout.jsx';
+import NewsMainInfo from './components/NewsMainInfo.jsx';
+
+import LogedDefaultLayout from "./views/LogedDefaultLayout.jsx";
+import PanelGłówny from "./views/PanelGółwny.jsx";
+import Ogloszenia from "./views/Ogłoszenia.jsx";
+import Karta from "./views/Karta.jsx";
+import Informacje from "./views/Informacje.jsx";
+
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <GuestLayout/>,
-        // children: [
-        //     {
-        //         path: 'newsy',
-        //         element: <NewsMainInfo/>
-        //     }
-        // ]
+        element: <LogedDefaultLayout />,
+        children: [
+            {
+                path: '/panelglowny',
+                element: <Navigate to= "/"/>
+            },
+            {
+                path: '/',
+                element: <PanelGłówny/>
+            },
+
+            {
+                path: 'ogloszenia',
+                element: <Ogloszenia/>
+            },
+            {
+                path: 'karta',
+                element: <Karta/>
+            },
+            {
+                path: 'informacje',
+                element: <Informacje/>
+            }
+        ]
     },
     {
         path: '/',
-        element: <Dashboard/>
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: 'news',
+                element: <NewsMainInfo/>
+            }
+        ]
     },
     {
         path: '/login',
--- react/src/views/GuestLayout.jsx
+++ react/src/views/GuestLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import GuestPanel from '../components/GuestPanel';
-import Login from './Login';
 import NewsMainInfo from '../components/NewsMainInfo';
 
 const GuestLayout = () => {
@@ -17,10 +16,7 @@ const GuestLayout = () => {
         {/* Wyświetlanie się pod spodem childrenów */}
 
         {/* Zrobić tu odwołania, a niżej przyciski do tego, min: Aktualności, o firmie, kontakt, routowanie w router.jsx, dać jako dzieci stroniy "/"*/}
-        <Routes>
-          <Route path="/news" element={<NewsMainInfo />} />
-
-        </Routes>
+        <Outlet />
       </main>
       <footer className="py-4 text-center text-sm text-white dark:text-White/70 bg-gray-500 w-full">
         E-firma
