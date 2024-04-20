cb6d171e7b27a08b279bb1f00da55d248b869da2
Agnieszka Ryś <ar12543@student.ans-ns.edu.pl>
2024-04-20 20:15:15 +0200

    zmiany

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('adduser', [AuthController::class, 'adduser']);
+Route::post('login', [AuthController::class, 'login']);
