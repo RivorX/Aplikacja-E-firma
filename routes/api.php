36ba5de485228e874f5b26e38e25f1fd35de6222
RivorX <rafilix11@gmail.com>
2024-04-21 12:37:23 +0200

    Działające wylogowanie

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\GuestNewsInfo;
 
-Route::get('/pracownik', function (Request $request) {
-    return $request->pracownik();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::post('adduser', [AuthController::class, 'adduser']);
 Route::post('login', [AuthController::class, 'login']);
+
+
 Route::get('positions', [AuthController::class, 'positions']);
 Route::get('GuestNewsInfo', [GuestNewsInfo::class, 'NewsInfo']);
