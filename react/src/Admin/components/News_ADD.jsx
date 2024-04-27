05eab7d06e92a05c7777e08d5fb179ceef3ccd75
Agnieszka Ryś <ar12543@student.ans-ns.edu.pl>
2024-04-27 22:05:51 +0200

    CRUD aktualności

--- react/src/Admin/components/News_ADD.jsx
+++ react/src/Admin/components/News_ADD.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import axiosClient from '../../axios';
+import { useNavigate, Link } from 'react-router-dom';
 
-export default function NewsForm() {
+
+export default function News_ADD() {
   const [title, setTitle] = useState("");
-  const [image, setImage] = useState(null);
   const [content, setContent] = useState("");
+  const navigate = useNavigate(); // hook do nawigacji
 
-  const handleImageChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setImage(selectedFile);
+  const handleSaveChanges = () => {
+    axiosClient.post('aktualnosci', {
+      tytul: title,
+      opis: content,
+    })
+    .then(({ data }) => {
+      console.log(data);
+      navigate('/admin/aktualnosci'); // przekierowanie po dodaniu aktualności
+    })
+    .catch((error) => {
+      console.error('Błąd dodawania aktualności:', error);
+    });
   };
 
   return (
@@ -15,12 +27,12 @@ export default function NewsForm() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Dodaj Aktualności
+            Dodaj Aktualność
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6">
             <div>
               <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
                 Tytuł
@@ -35,22 +47,6 @@ export default function NewsForm() {
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="image" className="block text-sm font-medium leading-6 text-gray-900">
-                Obraz
-              </label>
-              <div className="mt-2">
-                <input
-                  id="image"
-                  name="image"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  
                 />
               </div>
             </div>
@@ -73,15 +69,14 @@ export default function NewsForm() {
             </div>
 
             <div className="flex items-center justify-between mt-4">
-              <a href="/" className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
+              <Link to="/admin/aktualnosci" className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
                 Anuluj
-              </a>
-              <button
-                type="submit"
-                className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition"
-              >
-                Dodaj Aktualności
-              </button>
+              </Link>
+
+
+              <Link to="/admin/aktualnosci" onClick={handleSaveChanges} className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
+              Dodaj Aktualność
+              </Link>
             </div>
           </form>
         </div>
