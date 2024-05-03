ca50a5009fb885498c1210c61f2dbe3efcc8759b
RivorX <rafilix11@gmail.com>
2024-05-03 19:04:42 +0200

    Po pobraniu swaggera

--- react/src/Admin/components/News_UPDATE.jsx
+++ react/src/Admin/components/News_UPDATE.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axiosClient from '../../axios';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
 export default function News_UPDATE() {
   const { id } = useParams();
@@ -8,18 +8,23 @@ export default function News_UPDATE() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const [error, setError] = useState({ __html: "" });
+  const navigate = useNavigate(); 
+
   useEffect(() => {
+    setError({ __html: '' });
     // Pobierz dane konkretnej aktualności do edycji
     axiosClient.get(`aktualnosci/${id}`)
       .then(response => {
         setSelectedNews(response.data.news);
         setTitle(response.data.news.tytul);
         setContent(response.data.news.opis);
-        console.log("Tytuł:", title); // Dodajemy logowanie wartości stanów
-        console.log("Treść:", content);
       })
-      .catch(error => {
-        console.error('Błąd pobierania danych aktualności do edycji:', error);
+      .catch((error) => {
+        if (error.response && error.response.data && error.response.data.errors) {
+          const finalErrors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next], [])
+          setError({ __html: finalErrors.join('<br>') }); // Aktualizacja stanu error
+        }
       });
   }, [id]);
 
@@ -31,7 +36,11 @@ export default function News_UPDATE() {
       opis: content,
     })
     .then(({ data }) => {
-      console.log(data);
+      if (!data.error) {
+        navigate('/admin/aktualnosci');
+      } else {
+        console.error('Błąd Edycji Aktualności:', data.message);
+      }
     })
     .catch((error) => {
       if (error.response && error.response.data && error.response.data.errors) {
@@ -48,6 +57,11 @@ export default function News_UPDATE() {
           <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Edytuj Aktualności
           </h2>
+          {/* Wypisywananie błędów z backendu */}
+          {error.__html && (
+            <div className="bg-red-500 rounded py-2 px-3 text-white" dangerouslySetInnerHTML={error}>
+            </div>
+          )}
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -94,7 +108,7 @@ export default function News_UPDATE() {
               </Link>
 
 
-              <Link to="/admin/aktualnosci" onClick={handleSaveChanges} className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
+              <Link onClick={handleSaveChanges} className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
                 Zapisz zmiany
               </Link>
             </div>
