b055ddee48f84e9549d43f3a70ce1ffab7625fa3
Agnieszka Ryś <ar12543@student.ans-ns.edu.pl>
2024-05-04 21:01:28 +0200

    stworzenie CRUDa do drzwi oraz routing

---
+++ app/Http/Controllers/DrzwiController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Drzwi;
+
+class DrzwiController extends Controller
+{
+    public function index()
+    {
+        $drzwis = Drzwi::all();
+        return response()->json($drzwis);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Drzwi_id_Podstawowy' => 'required',
+            'nr_drzwi' => 'required',
+            'nazwa' => 'required',
+            'WeWy' => 'required',
+            'Strefy_Dostepu_id_Indeks' => 'required',
+            'drzwi_aktywne' => 'required',
+        ]);
+
+        $drzwi = Drzwi::create($request->all());
+        
+        return response()->json($drzwi, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'Drzwi_id_Podstawowy' => 'required',
+            'nr_drzwi' => 'required',
+            'nazwa' => 'required',
+            'WeWy' => 'required',
+            'Strefy_Dostepu_id_Indeks' => 'required',
+            'drzwi_aktywne' => 'required',
+        ]);
+
+        $drzwi = Drzwi::findOrFail($id);
+        $drzwi->update($request->all());
+
+        return response()->json($drzwi, 200);
+    }
+
+    public function destroy($id)
+    {
+        $drzwi = Drzwi::findOrFail($id);
+        $drzwi->delete();
+
+        return response()->json(null, 204);
+    }
+}
